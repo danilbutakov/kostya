767117af0f33a8781df24855c9b9e98e71375bda
Dani <dani@pop-os.localdomain>
2022-05-18 21:13:41 +0800

    Работа с admin.php29

--- adminI.php
+++ adminI.php
@@ -24,25 +24,25 @@ if (mysqli_connect_errno()) {
         <h3><a href="adminP.php">Редактирование par</a></h3>
         <h3><a href="admin.php">Редактирование h1</a></h3>
         <?php
-        $query = "SELECT * FROM h";
-        $row = mysqli_query($mysqli, $query);
-        foreach ($row as $rows) {
+        $query = "SELECT * FROM i";
+        $row2 = mysqli_query($mysqli, $query);
+        foreach ($row2 as $rows2) {
         }
         ?>
-        <form action="" method="post" class="admin">
-            <input type="text" name="h1" placeholder="<?= $rows['h1']; ?>">
+        <form action="" method="post" class="admin" enctype="multipart/form-data">
+            <input type="file" name="img"><?= $rows2['img']; ?>
             <input type="submit" value="Сохранить">
         </form>
         <?php
 
-        if ($_POST['h1'] != '') {
-            $h1 = $_POST['h1'];
+        if ($_POST['img'] != '') {
+            $img = $_POST['img'];
         } else {
-            $h1 = $rows['h1'];
+            $img = $rows2['img'];
         }
-        if (isset($h1)) {
-            $row = "UPDATE h SET h1='$h1' WHERE id=1";
-            $mysqli->query($row);
+        if (isset($img)) {
+            $row2 = "UPDATE i SET img='$img' WHERE id=1";
+            $mysqli->query($row2);
         }
         ?>
 
