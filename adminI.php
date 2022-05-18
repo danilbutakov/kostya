22785cff57c0e6580c322d8ee26117c7e921e2ba
Dani <dani@pop-os.localdomain>
2022-05-18 20:32:17 +0800

    Работа с admin.php23

--- adminI.php
+++ adminI.php
@@ -20,9 +20,9 @@ if (mysqli_connect_errno()) {
 
 <body>
     <div>
-        <h1>Редактирование h1</h1>
-        <h3><a href="">Редактирование par</a></h3>
-        <h3><a href="">Редактирование img</a></h3>
+        <h1><a href="adminI.php">Редактирование img</a></h1>
+        <h3><a href="adminP.php">Редактирование par</a></h3>
+        <h3><a href="admin.php">Редактирование h1</a></h3>
         <?php
         $query = "SELECT * FROM h";
         $row = mysqli_query($mysqli, $query);
