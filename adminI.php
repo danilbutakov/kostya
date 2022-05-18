6e419d37f4d57f21ed429512cfd1dca005c85224
Dani <dani@pop-os.localdomain>
2022-05-18 23:37:52 +0800

    Работа с admin.php51

--- adminI.php
+++ adminI.php
@@ -31,7 +31,7 @@ $result = mysqli_query($mysqli, "SELECT FROM `i`");
         </form>
         <?php
 
-        if (!empty($_FILES['file'])) {
+        if (empty($_FILES['file'])) {
             $file = $_FILES['file'];
             $name = $file['name'];
             $pathFile = __DIR__ . '/img/' . $name;
