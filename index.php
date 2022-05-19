9efc8b2d690dc9db43aed71ad113650e5b5a7af7
Dani <dani@pop-os.localdomain>
2022-05-20 01:48:10 +0800

    Работа с popup14

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ include "db.php";
             <script type="text/javascript">
                 window.onload = function() {
                     let button = document.getElementById('input_button_bg_change');
-                    let body = document.getElementsByTagName('body')[0];
+                    let form = document.getElementsByTagName('form')[0];
                     let colors = ['blue', 'grey', 'black', 'white', 'red', 'green', '#aaa', '#FFAACC', 'rgb(122,111,110)'];
                     button.onclick = function() {
                         form.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
