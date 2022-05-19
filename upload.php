d74208e77bc50d00cdba01f3cd45827b11a7ce56
Dani <dani@pop-os.localdomain>
2022-05-20 00:41:14 +0800

    Работа с загрузкой картинки7

--- upload.php
+++ upload.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit']) && isset($_POST['my_image'])) {
 
     $img_name = $_FILES['my_image']['name'];
     $img_size = $_FILES['my_image']['size'];
-    $img_size = $_FILES['my_image']['tmp_name'];
+    $tmp_name = $_FILES['my_image']['tmp_name'];
     $error = $_FILES['my_image']['error'];
 
     if ($error === 0) {
@@ -15,7 +15,18 @@ if (isset($_POST['submit']) && isset($_POST['my_image'])) {
             header("Loaction: adminI.php?error=$em");
         } else {
             $img_ex = pathinfo($img_name, PATHINFO_EXTENSION);
-            echo ($img_ex);
+            $img_ex_lc = strtolower($img_ex);
+
+            $allowed_exs = array("jpg", "jpeg", "png");
+
+            if (in_array($img_ex_lc, $allowed_exs)) {
+                $new_img_name = uniqid("IMG-", true) . '.' . $img_ex_lc;
+                $img_upload_path = 'uploads/' . $new_img_name;
+                move_uploaded_file($tmp_name, $img_upload_path);
+            } else {
+                $em = "Вы не можете загрузить файл этого типа!";
+                header("Loaction: adminI.php?error=$em");
+            }
         }
     } else {
         $em = "неизвестная ошибка!";
