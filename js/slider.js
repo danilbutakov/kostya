78bf33d231a5d92bb0df4f6dc818f6784afb65bb
Dani <dani@pop-os.localdomain>
2022-05-24 15:39:29 +0800

    Работа со сладйером28

--- js/slider.js
+++ js/slider.js
@@ -52,7 +52,7 @@ openPopUpImg.addEventListener('click', function(e) {
 });
 
 
-closePopUpImg.addEventListener('click', function(e) {
+popUpImg.addEventListener('click', function(e) {
     e.preventDefault();
     popUpImg.classList.remove('act');
 });
