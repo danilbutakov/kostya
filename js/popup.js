ac51cd2c18ac084b29a1e77f401f03408fe4f82c
Dani <dani@pop-os.localdomain>
2022-05-24 14:17:38 +0800

    Работа с js3

--- js/popup.js
+++ js/popup.js
@@ -3,22 +3,22 @@
 const openPopUp = document.getElementById('open__pop_up');
 const closePopUp = document.getElementById('close__pop_up');
 const popUp = document.getElementById('pop_up');
-const body = document.body || document.getElementsByTagName('body')[0];
+const popUpBody = document.getElementById('popup__container');
 
 openPopUp.addEventListener('click', function(e) {
     e.preventDefault();
     popUp.classList.add('_active');
 });
 
-body.addEventListener('click', function(e) {
+popUpBody.addEventListener('click', function(e) {
     e.preventDefault();
-    body.classList.add('_active');
+    popUpBody.classList.add('_active');
 });
 
 closePopUp.addEventListener('click', () => {
     popUp.classList.remove('_active');
 });
 
-body.addEventListener('click', () => {
-    body.classList.remove('_active');
+popUpBody.addEventListener('click', () => {
+    popUpBody.classList.remove('_active');
 });
