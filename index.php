b0871a190b901edf6968b51918d9648cbc5b0159
Dani <dani@pop-os.localdomain>
2022-05-24 15:57:53 +0800

    Работа со сладйером39

--- index.php
+++ index.php
@@ -11,6 +11,8 @@ include "db.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site</title>
     <link rel="stylesheet" href="css/style.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="jquery.maskedinput.min.js"></script>
 </head>
 
 <body>
@@ -286,8 +288,6 @@ include "db.php";
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4882.981548093942!2d104.26211414792905!3d52.27079157812597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5da824a8ee4f155d%3A0x368423aaef9b2b13!2z0JjRgNC60YPRgtGB0LrQuNC5INCz0L7RgdGD0LTQsNGA0YHRgtCy0LXQvdC90YvQuSDRg9C90LjQstC10YDRgdC40YLQtdGCINC_0YPRgtC10Lkg0YHQvtC-0LHRidC10L3QuNGP!5e0!3m2!1sru!2sru!4v1652779756938!5m2!1sru!2sru" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
     </div>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-    <script src="jquery.maskedinput.min.js"></script>
     <script src="js/popup.js"></script>
     <script src="js/slider.js"></script>
 </body>
